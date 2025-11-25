15be68d7eaf8664e814d8172ae5dda95b48a3580
Hossein Mirjalali <mirjalali.hosein@gmail.com>
2025-11-25 19:08:30 +1100

    Fix the linenumbers changing after visual mode

--- lua/configs/mini.lua
+++ lua/configs/mini.lua
@@ -55,7 +55,7 @@ M.basics = {
     -- Basic autocommands (highlight on yank, start Insert in terminal, ...)
     basic = true,
     -- Set 'relativenumber' only in linewise and blockwise Visual mode
-    relnum_in_visual_mode = true,
+    relnum_in_visual_mode = false,
   },
 }
 
